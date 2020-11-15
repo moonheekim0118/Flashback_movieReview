26b9e50d3962d0ea3b681525eb0d7577265a3247
moonee <uuuu118@naver.com>
2020-11-15 15:14:45 +0900

    feat : 버튼컴포넌트 구현

---
+++ front/src/atoms/Buttons/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface Props{
+    fill?:boolean;
+    shadow?:boolean;
+    color?:string;
+    onClick?: (e: React.MouseEvent) => void;
+    title?:string;
+}
+
+export default function index({ fill=false, shadow=false, color="#e6b3cc", onClick,title } : Props){
+    return(
+        <Button fill={fill.toString()} shadow={shadow.toString()} color={color} onClick={onClick}>
+            {title}
+        </Button>
+    );
+}
+
+const Button = styled.button<{ fill:string , color:string}>
+`
+    width:${(props)=>props.fill ==="true"? '100%':''};
+    padding: 10px 15px;
+
+    border:none;
+    border-radius:5px;
+    
+    font-size:1.2rem;
+    color:${(props)=>props.color === "#cc00cc" ? '#fff' : 'black'};
+    background-color:${(props)=>props.color};
+
+    box-shadow:${(props)=>props.shadow==="true"? '0px 0px 11px -1px rgba(0,0,0,0.75)' :''};
+    cursor:pointer;
+    
+`;
