ef7cdaba1e325f7b7eaefc075714fd96df69c65c
moonee <uuuu118@naver.com>
2020-11-18 17:27:56 +0900

    fix : 아이콘 색상 변경하도록 수정 - color props로 받아와서 컴포넌트 외부에서 색상 설정할 수 있도록 변경

--- front/src/atoms/Icons/index.tsx
+++ front/src/atoms/Icons/index.tsx
@@ -4,30 +4,33 @@ import {
     FontAwesomeIcon,
     FontAwesomeIconProps
 } from '@fortawesome/react-fontawesome';
+import { colorCode } from '../../util/color';
 
 interface Props {
     size?:number;
     icon?:FontAwesomeIconProps['icon'];
     className?:string;
+    color?:string;
     onClick?: (e: React.MouseEvent) => void;
 }
 
-const Icon=({ size = 24 , icon, className, onClick=null}: Props)=>{
+const Icon=({ size = 24 , icon, className, color="purple", onClick=null}: Props)=>{
     return(
        <StyledIcon
         iconsize={size}
         icon={icon}
         className={className}
+        color={color}
         onClick={onClick}
        />
     );
 }
 
-const StyledIcon = styled<{ iconsize: number}>(FontAwesomeIcon)`
+const StyledIcon = styled<{ iconsize: number , color:string}>(FontAwesomeIcon)`
     width:${(props)=>props.iconsize}px;
     height:${(props)=>props.iconsize}px;
     font-size: ${(props)=>props.iconsize}px;
-    color:#cc00cc;
+    color:${(props)=>colorCode[`${props.color}`]};
     cursor:pointer;
 
     transition: 0.2s color ease-in-out;
