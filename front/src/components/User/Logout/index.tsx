25a0a908ae35d2d3c3d10e804bf3e65b5609d6c8
moonee <uuuu118@naver.com>
2020-11-19 14:03:38 +0900

    feat : 로그아웃 컴포넌트 구현

---
+++ front/src/components/User/Logout/index.tsx
@@ -0,0 +1,27 @@
+import React , { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { LOGOUT_REQUEST } from '../../../actions/user';
+import Icon from '../../../atoms/Icons';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+
+const Logout=()=>{
+    const dispatch = useDispatch();
+
+    const onClick=useCallback(()=>{
+        dispatch({
+            type:LOGOUT_REQUEST
+        })
+    },[]);
+
+    return(
+        <Icon
+        icon={faSignOutAlt}
+        onClick={onClick}
+        color={"lightPurple"}
+        className={"faSignOutAlt"}
+        />
+    );
+}
+
+
+export default Logout;
