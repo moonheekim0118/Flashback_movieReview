ddfa440703e3b20cf36090af6b630ce81aa34a04
moonee <uuuu118@naver.com>
2020-11-25 13:06:13 +0900

    feat : 삭제 후 삭제완료 alert 구현

--- front/src/components/Review/Preview/index.tsx
+++ front/src/components/Review/Preview/index.tsx
@@ -1,12 +1,13 @@
-import React , { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import React , { useEffect, useState, useCallback } from 'react';
+import { useDispatch , useSelector } from 'react-redux';
 import Router from 'next/router';
 import styled from 'styled-components';
 import Badge from '../Badge';
 import Icon from '../../../atoms/Icons';
 import ConfirmAlert from '../../ConfirmAlert';
 import useAlert from '../../../hooks/useAlert';
-import { REMOVE_MY_REVIEW_REQUEST } from '../../../actions/review';
+import Alert from '../../Alert';
+import { REMOVE_MY_REVIEW_REQUEST , INIT_REMOVE } from '../../../actions/review';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { ReviewList } from '../../../model/ReviewList';
 import { Container,MoviePoster,MovieDescription } from '../../Movie/MovieCard';
@@ -17,29 +18,54 @@ interface Props {
 
 const Preview=({Review}:Props)=>{
     const dispatch = useDispatch();
+    const [ alertMessage , setAlertMessage ] = useState("삭제 되었습니다.");
     const [ showConfirmAlert, openConfirmAlert, closeConfirmAlert ] = useAlert();
+    const [ showDoneAlert, openDoneAlert, closeDoneAlert ] = useAlert();
+    const { removeMyReviewDone, removeMyReviewError  } = useSelector((state)=>state.review);
+
+    useEffect(()=>{
+        if(removeMyReviewDone){
+            openDoneAlert();
+            const timer = setTimeout(closeDoneAlert,2000);
+            return ()=>{
+                clearTimeout(timer);
+                dispatch({type:INIT_REMOVE});
+            }
+        }
+        else if(removeMyReviewError){
+            setAlertMessage(removeMyReviewError);
+            const timer = setTimeout(closeDoneAlert,2000);
+            return ()=>{
+                clearTimeout(timer);
+                dispatch({type:INIT_REMOVE});
+            }
+        }
+    },[removeMyReviewDone,removeMyReviewError]);
+
     const onMove = useCallback(()=>{
         Router.push(`/singleReview/${Review.id}`);
     },[]);
 
-    const openRemoveAlert = useCallback((e)=>{
+    
+    const openRemoveAlert = useCallback((e)=>{ // alert 띄워주기 
         e.stopPropagation();
         openConfirmAlert();
     },[]);
 
-    const closeRemoveAlert = useCallback((e)=>{
+    const closeRemoveAlert = useCallback((e)=>{ // alert 닫아주기 
         e.stopPropagation();
         closeConfirmAlert();
     },[]);
 
-    const onClickRemove = useCallback((e)=>{
-        e.stopPropagation();
+    const onClickRemove = useCallback((e)=>{ // 리뷰 삭제 
+        e.stopPropagation(); 
         dispatch({type:REMOVE_MY_REVIEW_REQUEST, data:Review.id});
         closeConfirmAlert();
     },[]);
 
     return(
         <Container onClick={onMove}>
+            {showDoneAlert && <Alert text={alertMessage}/>}
             {showConfirmAlert && <ConfirmAlert text={"정말 삭제하시겠습니까?"} clickYes={onClickRemove} clickNo={closeRemoveAlert}/>}
             <MoviePoster src={Review.Movie.image}/>
             <MovieDescription>
