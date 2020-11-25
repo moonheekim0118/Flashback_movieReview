d5601e1783e9026d8ea4c7264dbf2f40b5a06ea9
moonee <uuuu118@naver.com>
2020-11-25 10:37:24 +0900

    feat : 삭제 confirmAlert 구현 - 삭제아이콘 누르면 삭제 confirm Alert 띄워주도록 구현 - confirm Alert는 모달형태로 구현

--- front/src/components/Review/Preview/index.tsx
+++ front/src/components/Review/Preview/index.tsx
@@ -1,26 +1,51 @@
 import React , { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
 import Router from 'next/router';
 import styled from 'styled-components';
 import Badge from '../Badge';
+import Icon from '../../../atoms/Icons';
+import ConfirmAlert from '../../ConfirmAlert';
+import useAlert from '../../../hooks/useAlert';
+import { REMOVE_MY_REVIEW_REQUEST } from '../../../actions/review';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { ReviewList } from '../../../model/ReviewList';
 import { Container,MoviePoster,MovieDescription } from '../../Movie/MovieCard';
 
 interface Props {
-    Review?:ReviewList
+    Review?:ReviewList;
 }
 
 const Preview=({Review}:Props)=>{
-    const onClick = useCallback(()=>{
+    const dispatch = useDispatch();
+    const [ showConfirmAlert, openConfirmAlert, closeConfirmAlert ] = useAlert();
+    const onMove = useCallback(()=>{
         Router.push(`/singleReview/${Review.id}`);
     },[]);
 
+    const openRemoveAlert = useCallback((e)=>{
+        e.stopPropagation();
+        openConfirmAlert();
+    },[]);
+
+    const closeRemoveAlert = useCallback((e)=>{
+        e.stopPropagation();
+        closeConfirmAlert();
+    },[]);
+
+    const onClickRemove = useCallback(()=>{
+        dispatch({type:REMOVE_MY_REVIEW_REQUEST, data:Review.id});
+        closeConfirmAlert();
+    },[]);
+
     return(
-        <Container onClick={onClick}>
+        <Container onClick={onMove}>
+            {showConfirmAlert && <ConfirmAlert text={"정말 삭제하시겠습니까?"} clickYes={onClickRemove} clickNo={closeRemoveAlert}/>}
             <MoviePoster src={Review.Movie.image}/>
             <MovieDescription>
                 <Comment>{Review.shortComment}</Comment>
                 <Badge badgeName={Review.rating} selected={true}/>
             </MovieDescription>
+            <Icon icon={faTrash} className={"faTrash"} onClick={openRemoveAlert}/>
         </Container>
     );
 }
