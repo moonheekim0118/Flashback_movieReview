6538f4e6ebe00dccb00e6b5e0bdc777a08634a9b
moonee <uuuu118@naver.com>
2020-12-02 13:51:14 +0900

    chore : script 추가 및 db config 설정 변경

--- back/config/config.js
+++ back/config/config.js
@@ -6,7 +6,6 @@ module.exports={
       "username": "root",
       "password": process.env.DB_PASSWORD,
       "database": "movie_review",
-      "port":3360,
       "host": "127.0.0.1",
       "dialect": "mysql"
     },
