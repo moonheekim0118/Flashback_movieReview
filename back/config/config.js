d485d819c43358f23699db3d269cd2e1b982107a
moonee <uuuu118@naver.com>
2020-12-02 14:32:08 +0900

    chore : config 파일 수정

--- back/config/config.js
+++ back/config/config.js
@@ -18,7 +18,7 @@ module.exports={
     },
     "production": {
       "username": "root",
-      "password": null,
+      "password": process.env.DB_PASSWORD,
       "database": "movie_review",
       "host": "127.0.0.1",
       "dialect": "mysql"
