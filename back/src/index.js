3b0f7e714fbfd5eb09349bedec4953a28bec4448
moonee <uuuu118@naver.com>
2020-12-02 16:00:29 +0900

    fix typo

--- back/src/index.js
+++ back/src/index.js
@@ -54,7 +54,6 @@ app.use(session({
     }
 }));
 app.use(passport.initialize());
-app.use(passport.session());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use('/',express.static(path.join(__dirname, '/../uploads')));
