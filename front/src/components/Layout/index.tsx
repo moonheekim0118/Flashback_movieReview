ee0df3952bdfaa1085376d95ab60481ee3028a66
moonee <uuuu118@naver.com>
2020-12-14 14:45:51 +0900

    refactor : header 컴포넌트 분리 - Layout 컴포넌트로부터 header 컴포넌트 분리

--- front/src/components/Layout/index.tsx
+++ front/src/components/Layout/index.tsx
@@ -1,13 +1,9 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { closeAlertAction } from '../../actions/alert';
-import { faChevronLeft, faBars } from '@fortawesome/free-solid-svg-icons';
-import Link from 'next/link';
-import Router from 'next/router';
 import Alert from '../../components/Alert';
-import Icons from '../../atoms/Icons';
+import Header from '../../components/Header';
 import styled from 'styled-components';
-import Logout from '../User/Logout';
 
 interface Props {
   PageName?: string;
@@ -19,10 +15,6 @@ const Layout = ({ PageName = '', children = null }: Props) => {
   const { showAlert } = useSelector((state) => state.alert);
   const loginDone = useSelector((state) => state.user.loginDone);
 
-  const onPushBack = useCallback(() => {
-    Router.back();
-  }, []);
-
   useEffect(() => {
     if (showAlert) {
       // Alert 띄워주는 요청이 들어오면 5초 후에 닫아준다.
@@ -31,36 +23,10 @@ const Layout = ({ PageName = '', children = null }: Props) => {
     }
   }, [showAlert]);
 
-  // 페이지 네임이 메뉴인경우에는 로그아웃 버튼을 띄워주고 그 외에 경우에는 메뉴 아이콘을 띄워준다.
-  const MainButton =
-    PageName === '메뉴' && loginDone ? (
-      <Logout />
-    ) : (
-      <Link href="/menu">
-        <a>
-          <Icons icon={faBars} className="faBars" color="lightPurple" />
-        </a>
-      </Link>
-    );
-
   return (
     <App>
       <Alert />
-      {PageName && (
-        <Header>
-          <div>
-            <Icons
-              icon={faChevronLeft}
-              className="faChevronLeft"
-              onClick={onPushBack}
-            />
-          </div>
-          <PageInfo>
-            <PageDescription>{PageName}</PageDescription>
-            {MainButton}
-          </PageInfo>
-        </Header>
-      )}
+      {PageName && <Header PageName={PageName} loginDone={loginDone} />}
       {children}
     </App>
   );
@@ -82,41 +48,6 @@ const App = styled.div`
   @media screen and (max-width: 768px) {
     width: 375px;
   } ;
-`;
-const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  width: 100%;
-  height: 50px;
-  padding: 0px 20px;
-
-  position: sticky;
-  top: 0;
-
-  color: #cc00cc;
-  border-bottom: 1px solid #e6b3cc;
-  background-color: inherit;
-
-  z-index: 2000;
-`;
-
-const PageInfo = styled.div`
-  max-width: 700px;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  overflow: ellipsis;
-
-  @media screen and (max-width: 768px) {
-    max-width: 190px;
-  } ;
-`;
-
-const PageDescription = styled.div`
-  width: 100%;
-  margin-right: 20px;
 `;
 
 export default Layout;
