64e3656c414ba64aa6e6789b51e12de6733d7b92
moonee <uuuu118@naver.com>
2020-12-16 13:03:17 +0900

    fix : util함수 type 설정

--- front/src/util/movieParser.ts
+++ front/src/util/movieParser.ts
@@ -1,10 +1,10 @@
-export const titleParser = (title) => {
+export const titleParser = (title: string): string => {
   let newTitle = title.replace(/<b>/g, '');
   newTitle = newTitle.replace(/[</b>]/g, '');
   return newTitle;
 };
 
-export const directorParser = (director) => {
+export const directorParser = (director: string): string => {
   director = director.replace(/[|]/g, '');
   return director;
 };
